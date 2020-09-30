5607a57f300b542d48e4937f612d325c7b8a7993
Makar-1 <72124777+Makar-1@users.noreply.github.com>
2020-09-30 10:03:38 +0300

    Add files via upload

---
+++ laba15_2.cpp
@@ -0,0 +1,27 @@
+﻿// laba15_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <locale>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int x, a[3], k, i = 0;
+	for (int j = 0; j < 3; j++) {
+		do {
+			system("cls");
+			printf("Введите число %d: ", i+1);
+			k = scanf_s("%d", &x);
+		} while (k == 0);
+		if (x > 0)
+			a[i] = x * x;
+		else
+			a[i] = x;
+		i++;
+	}
+	for (int j = 0; j < 3; j ++) {
+		std::cout << a[j] << std::endl;
+	}
+}
